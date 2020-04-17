674b116773ffa5f78a8556dfd6431473e500c902
LiquidGianmix <luca.arrighetti@gmail.com>
2020-04-18 01:21:04 +0200

    Creating the menu...
    skelly menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,56 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Emperors of the Galaxy");
+        System.out.println("Hello Emperors of the Galaxy!");
+        System.out.println("\n*** WELCOME TO THE  P L A N E T A R I U M ***");
+        System.out.println("" +
+                "\nGreat things you can do:" +
+                "\n1. Take a look at the existing planets" +
+                "\n2. Add a new planet" +
+                "\n3. Remove an existing planet" +
+                "\n4. Take a look at the existing moons" +
+                "\n5. Add a new moon" +
+                "\n6. Remove an existing moon" +
+                "\n7. Center of mass");
+        System.out.println("+++ ENTER A NUMBER TO SELECT AN OPTION +++");
+
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+
+        switch (choice) {
+            case 1: {
+
+                break;
+            }
+            case 2: {
+
+                break;
+            }
+            case 3: {
+
+
+                break;
+            }
+            case 4: {
+
+                break;
+            }
+            case 5: {
+
+                break;
+            }
+            case 6: {
+
+                break;
+            }
+            case 7: {
+
+                break;
+            }
+        }
     }
 }
